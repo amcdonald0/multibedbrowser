298de5c61677d8fcd81c1a02ebdf452810cc1fed
Riya <jadhavriya2000@gmail.com>
2025-04-21 23:21:43 +0000

    kill me

--- templates/upload.py
+++ templates/upload.py
@@ -1,10 +1,11 @@
 
 from flask import Flask, render_template, request
-#import json
+import sys
 import mariadb
 
 app = Flask(__name__)
-
+def home():
+    return render_template("upload.html")
 # Connect to MariaDB
 def get_db_connection():
     conn = mariadb.connect(
@@ -16,7 +17,17 @@ def get_db_connection():
     return conn
 
 @app.route('/')
-def index():
+def studies(/get_studies):
+    study_name = request.args.get("study_name","").strip()
+    file_name = request.args.get("file_name","").strip()
+    file_path = request.args.get("final_filepath","").strip()
+    file_type = request.args.get("file_size","").strip()
+    upload_date = request.args.get("upload_date","").strip() # use json timestamp function
+    description = request.args.get("description","").strip()
+    study_id = request.args.get("study_id","").strip()
+    species = request.args.get("species","").strip()
+    genome_release = request.args.get("genome_release","").strip()
+    experiment_type = request.args.get("experiment_type","").strip()
     conn = get_db_connection()
     cur = conn.cursor()
 
@@ -30,6 +41,14 @@ def index():
     cur.execute(query2)
     studies = cur.fetchall()
 
+    # insert file
+    query3 = """SELECT study_id WHERE study_name = %s"""
+    cur.execute(query3,[study_name])
+
+    query4 = """
+    INSERT INTO files (file_name, file_path, file_size, file_type, upload_date, description, study_id, species, genome_release, experiment_type)
+    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    cur.execute(query4,[file_name,file_path, file_size, file_type, upload_date, description, study_id, species, genome_release, experiment_type])
     conn.close()
 
     return render_template('upload.html', projects=projects, studies=studies)
