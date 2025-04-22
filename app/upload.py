217d56398e845a7368e2cf1f04ea0575514e1b02
Riya <jadhavriya2000@gmail.com>
2025-04-22 22:39:04 +0000

    some changes

--- app/upload.py
+++ app/upload.py
@@ -2,26 +2,33 @@
 from flask import Flask, render_template, request
 import sys
 import mariadb
+from app import app
 
-app = Flask(__name__)
+def get_db_connection():
+    try:
+        conn = mariadb.connect(
+            user='jriya186',
+            password='mushky1864',
+            host='bioed-new.bu.edu',
+            database='Team14', 
+            port=4253
+        )
+        conn.row_factory = mariadb.Row  # Optional, for dict-like access to rows
+        return conn
+    except mariadb.Error as e:
+        print(f"Error connecting to MariaDB: {e}")
+        return None
+    
+@app.route('/')
 def home():
     return render_template("upload.html")
-# Connect to MariaDB
-def get_db_connection():
-    conn = mariadb.connect(
-        user='jriya186',
-        password='mushky1864',
-        host='bioed-new.bu.edu',
-        database='Team14'
-    )
-    return conn
 
 @app.route('/')
 def studies(/get_studies):
     study_name = request.args.get("study_name","").strip()
     file_name = request.args.get("file_name","").strip()
     file_path = request.args.get("final_filepath","").strip()
-    file_type = request.args.get("file_size","").strip()
+    #file_type = request.args.get("file_size","").strip()
     upload_date = request.args.get("upload_date","").strip() # use json timestamp function
     description = request.args.get("description","").strip()
     study_id = request.args.get("study_id","").strip()
@@ -32,12 +39,12 @@ def studies(/get_studies):
     cur = conn.cursor()
 
     # Fetch projects
-    query1 = """SELECT project_id, project_name FROM Projects"""
+    query1 = """SELECT project_name FROM Projects"""
     cur.execute(query1)
     projects = cur.fetchall()
 
     # Fetch studies
-    query2 = """SELECT study_id, stduy_name FROM Studies"""
+    query2 = """SELECT study_name FROM Studies"""
     cur.execute(query2)
     studies = cur.fetchall()
 
@@ -46,9 +53,9 @@ def studies(/get_studies):
     cur.execute(query3,[study_name])
 
     query4 = """
-    INSERT INTO files (file_name, file_path, file_size, file_type, upload_date, description, study_id, species, genome_release, experiment_type)
-    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
-    cur.execute(query4,[file_name,file_path, file_size, file_type, upload_date, description, study_id, species, genome_release, experiment_type])
+    INSERT INTO files (file_name, file_path, file_size, upload_date, description, study_id, species, genome_release, experiment_type)
+    VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)"""
+    cur.execute(query4,[file_name,file_path, file_size, upload_date, description, study_id, species, genome_release, experiment_type])
     conn.close()
 
     return render_template('upload.html', projects=projects, studies=studies)
